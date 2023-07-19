5b0ac9456135155ae490d062f9c3555b1b935ef0
Derry Bradley <derrybradley@live.ie>
2023-07-19 20:30:04 +0200

    trigger build process now that repo is public

--- src/scripts/greet.sh
+++ src/scripts/greet.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 # This example uses envsubst to support variable substitution in the string parameter type.
 # https://circleci.com/docs/orbs-best-practices/#accepting-parameters-as-strings-or-environment-variables
-# TO=$(circleci env subst "${PARAM_TO}")
+TO=$(circleci env subst "${PARAM_TO}")
 
 HELLO=$(circleci env subst "${HELLO_WORLD}")
 # If for any reason the TO variable is not set, default to "World"
-# echo "Hello ${TO:-World}!"
 
-echo "Hello ${HELLO}!"
+echo "Hello ${TO:-World}!"
+
+echo "************************"
+
+echo "George ${HELLO}!"
 
 
